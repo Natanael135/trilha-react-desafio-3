c6d80cc5ddbaaf57a3ae063aac2d770696ed4f81
Natanael Melo <natanael.melo417@gmail.com>
2024-09-19 13:51:12 -0300

    correção da rota Feed e alteração de conteudo do Feed e alteração de users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Feed } from './pages/feed'
-
+import { Cadastro } from './pages/cadastro'
 import { Home } from './pages/home'
 import { Login } from './pages/login'
-import { Cadastro } from './pages/cadastro'
 import { GlobalStyle } from './styles/global'
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-
+        <Route path="/feed" element={<Feed />} />
         <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </Router>
--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -26,10 +26,10 @@ const Feed = () => {
             </Column>
             <Column flex={1}>
               <TitleHighlight> # RANKING 5 TOP DA SEMANA </TitleHighlight>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={25}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={65}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={45}/>
-                <UserInfo nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=" percentual={72}/>
+                <UserInfo nome="Natanael Melo" image="https://avatars.githubusercontent.com/u/109748470?s=400&u=0404c8f169e445cbb0c973cf2d23f40233d65cdd&v=4" percentual={25}/>
+                <UserInfo nome="Natanael Melo" image="https://avatars.githubusercontent.com/u/109748470?s=400&u=0404c8f169e445cbb0c973cf2d23f40233d65cdd&v=4" percentual={65}/>
+                <UserInfo nome="Natanael Melo" image="https://avatars.githubusercontent.com/u/109748470?s=400&u=0404c8f169e445cbb0c973cf2d23f40233d65cdd&v=4" percentual={45}/>
+                <UserInfo nome="Natanael Melo" image="https://avatars.githubusercontent.com/u/109748470?s=400&u=0404c8f169e445cbb0c973cf2d23f40233d65cdd&v=4" percentual={72}/>
             </Column>
         </Container>
     </>
